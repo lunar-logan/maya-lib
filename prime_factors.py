ffd5d29090f6b614c05cdaad95f3947c58b8cce9
Anurag <developerx@live.com>
2013-07-08 01:36:34 +0530

    Update prime_factors.py

--- prime_factors.py
+++ prime_factors.py
@@ -1,13 +1,14 @@
-def pf(n):
-  z = 2
-	p = set()
-	p.add(1)
-	while z*z <= n:
-		if n%z == 0:
-			if not z in p:
-				p.add(z)
-			n/=z
-		else:
-			z += 1
-	if n > 1: p.add(n)
-	return p
+import collections
+def pf(x):
+	i, c, factors = 0, x, collections.Counter()
+	while c%2 == 0:
+		factors[2] += 1
+		c /= 2
+	i=3
+	while i <= int(pow(c, 0.5)+1):
+		if c % i == 0:
+			factors[i] += 1
+			c /= i
+		else: i += 2
+	if c > 1: factors[c] += 1
+	return factors
